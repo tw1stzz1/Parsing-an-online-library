64b0e594f4393ffbcd2e2efdb4bd947342d649f7
tw1stzz1 <73423742+tw1stzz1@users.noreply.github.com>
2023-10-06 17:32:34 +0300

    update code

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import os
 import requests
 import argparse
 from pathlib import Path
+
+import time
 from urllib.parse import urljoin
 from urllib.parse import urlsplit
-
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
 
@@ -32,26 +33,28 @@ def download_image(img_url):
 
 
 def parse_book_page(soup):
-    book_info = {}
+    book_parameters = {}
     raw_genre = soup.find_all(class_='d_book')[1].text
     genre = raw_genre.split(':')[1].strip()
-    book_info['genre'] = (genre)
 
     title_text = soup.find(id='content').find('h1').text
     title = title_text.split('::')
     title = title[0].strip()
-    book_info['title'] = (title)
 
     raw_img = soup.find(class_='bookimage').find('img')['src']
-    img_url = urljoin("https://tululu.org", raw_img)
-    download_image(img_url)
-    book_info['img_url'] = img_url
+    img_url = urljoin("https://tululu.org/shots/", raw_img)
     
+    book_parameters = {
+        'genre' : genre,
+        'title' : title,
+        'img_url' : img_url,
+    }
+
     raw_comment = soup.find(id='content').find_all(class_="texts")
     for comment in raw_comment:
         comment = comment.find('span').text
-        book_info['comments'] = comment
-    return book_info
+        book_parameters['comments'] = comment
+    return book_parameters
 
 
 def check_for_redirect(response):
@@ -63,33 +66,40 @@ def main():
     Path("books").mkdir(parents=True, exist_ok=True)
     Path("images").mkdir(parents=True, exist_ok=True)
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--start_id', default=1)
-    parser.add_argument('--end_id', default=11)
+    parser = argparse.ArgumentParser(description='This code allows you to download books and their covers form tululu')
+    parser.add_argument('--start_id', default=1, help='Id of the book from which the download will begin')
+    parser.add_argument('--end_id', default=11, help='Id of the book where the download will end')
     args = parser.parse_args()
 
-    url = "https://tululu.org/txt.php"
+    book_txt_url = "https://tululu.org/txt.php"
 
     for book_id in range(int(args.start_id), int(args.end_id)):
         params = {
             "id": book_id
         }
         try:
-            response = requests.get(url, params=params)
+            response = requests.get(book_txt_url, params=params)
             response.raise_for_status()
             check_for_redirect(response)
 
-            url_for_soup = f"https://tululu.org/b{book_id}"
-            answer = requests.get(url_for_soup)
+            book_url = f"https://tululu.org/b{book_id}"
+            answer = requests.get(book_url)
             answer.raise_for_status()
+            check_for_redirect(answer)
 
             soup = BeautifulSoup(answer.text, 'lxml')
-            book_info = parse_book_page(soup)
-            title = book_info['title']
+            book_parameters = parse_book_page(soup)
+            title = book_parameters['title']
+            img_url = book_parameters['img_url']
+            download_image(img_url)
 
             download_book(book_id, response, title)
+        except requests.exceptions.ConnectionError:
+            print("Разрыв соединения с сайтом")
+            time.sleep(20)
         except requests.exceptions.HTTPError:
             print("Такой книги нет!", book_id)
+
 
 
 if __name__ == "__main__":
