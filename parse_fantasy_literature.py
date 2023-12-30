0cac3add7840582c2af7386ce78060d886f5b8a5
Tkaz <73423742+tw1stzz1@users.noreply.github.com>
2023-12-30 15:53:37 +0300

    tools added

--- parse_fantasy_literature.py
+++ parse_fantasy_literature.py
@@ -6,75 +6,78 @@ import argparse
 from pathlib import Path
 
 from bs4 import BeautifulSoup
-from parse_business_literature import download_book, download_image, parse_book_page, check_for_redirect
+from tools import download_book, download_image, parse_book_page, check_for_redirect
 
+def main():
+    parser = argparse.ArgumentParser(description='This code allows you to download books and their covers form tululu')
+    parser.add_argument('--start_page', default=1, help='Page from which the download will begin')
+    parser.add_argument('--end_page', default=4, help='Page where the download will end')
+    parser.add_argument('--skip_imgs', default=False, help='This setting skips image download')
+    parser.add_argument('--skip_txt', default=False, help='This setting skips txt file download')
+    parser.add_argument('--dest_folder', default="result", help='This setting skips txt file download')
+    args = parser.parse_args()
 
-parser = argparse.ArgumentParser(description='This code allows you to download books and their covers form tululu')
-parser.add_argument('--start_page', default=1, help='Page from which the download will begin')
-parser.add_argument('--end_page', default=4, help='Page where the download will end')
-parser.add_argument('--skip_imgs', default=False, help='This setting skips image download')
-parser.add_argument('--skip_txt', default=False, help='This setting skips txt file download')
-parser.add_argument('--dest_folder', default="result", help='This setting skips txt file download')
-args = parser.parse_args()
+    Path(args.dest_folder).mkdir(parents=True, exist_ok=True)
 
-Path(args.dest_folder).mkdir(parents=True, exist_ok=True)
+    book_txt_url = "https://tululu.org/txt.php"
+    dest_folder = args.dest_folder
+    for nunbers in range(int(args.start_page), int(args.end_page)):
+        url = f"https://tululu.org/l55/{nunbers}/"
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'lxml')
+        books_json_parameters = []
+        books = soup.find_all(class_='d_book')
+        try:
+            for book_id in books:
+                book_id = book_id.find('a')['href']
+                book_url = urljoin(url, book_id)
+                book_id = book_id[2:-1]
 
-book_txt_url = "https://tululu.org/txt.php"
-dest_folder = args.dest_folder
-for nunbers in range(int(args.start_page), int(args.end_page)):
-    url = f"https://tululu.org/l55/{nunbers}/"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'lxml')
-    books_json_parameters = []
-    books = soup.find_all(class_='d_book')
-    try:
-        for book_id in books:
-            book_id = book_id.find('a')['href']
-            book_url = urljoin(url, book_id)
-            book_id = book_id[2:-1]
+                answer = requests.get(book_url)
+                answer.raise_for_status()
+                check_for_redirect(answer)
+                
+                book_parameters = parse_book_page(answer, book_url)
+                title = book_parameters['title']
+                img_url = book_parameters['img_url']
+                if not args.skip_imgs:
+                    img_path = download_image(img_url, dest_folder)
+                else:
+                    img_path = None
+                
 
-            answer = requests.get(book_url)
-            answer.raise_for_status()
-            check_for_redirect(answer)
-            
-            book_parameters = parse_book_page(answer, book_url)
-            title = book_parameters['title']
-            img_url = book_parameters['img_url']
-            if not args.skip_imgs:
-                img_path = download_image(img_url, dest_folder)
-            else:
-                img_path = None
-            
+                if not args.skip_txt:
+                    params = {
+                        "id": book_id
+                    }
+                    response = requests.get(book_txt_url, params=params)
+                    check_for_redirect(response)
 
-            if not args.skip_txt:
-                params = {
-                    "id": book_id
+                    book_path = download_book(book_id, response, title, dest_folder)
+                else:
+                    book_path = None
+                
+                book_json_parameters = {
+                'title': title,
+                'author' : book_parameters['author'],
+                'img_src' : img_path,
+                'book_path' : book_path,
+                'genres' : book_parameters['genres'],
+                'comments' : book_parameters['comments']
                 }
-                response = requests.get(book_txt_url, params=params)
-                check_for_redirect(response)
-
-                book_path = download_book(book_id, response, title, dest_folder)
-            else:
-                book_path = None
-            
-            book_json_parameters = {
-            'title': title,
-            'author' : book_parameters['author'],
-            'img_src' : img_path,
-            'book_path' : book_path,
-            'genres' : book_parameters['genres'],
-            'comments' : book_parameters['comments']
-            }
-            
-            books_json_parameters.append(book_json_parameters)
-    except requests.exceptions.ConnectionError:
-        print("Разрыв соединения c сайтом")
-        time.sleep(20)
-    except requests.exceptions.HTTPError:
-        print("Такой книги нет!", book_id)
+                
+                books_json_parameters.append(book_json_parameters)
+        except requests.exceptions.ConnectionError:
+            print("Разрыв соединения c сайтом")
+            time.sleep(20)
+        except requests.exceptions.HTTPError:
+            print("Такой книги нет!", book_id)
 
 
-books_json_parameters = json.dumps(books_json_parameters, ensure_ascii=False)
+    books_json_parameters = json.dumps(books_json_parameters, ensure_ascii=False)
 
-with open(f"{args.dest_folder}/books_json_parameters.json", "w", encoding='utf8') as my_file:
-    my_file.write(books_json_parameters)
+    with open(f"{args.dest_folder}/books_json_parameters.json", "w", encoding='utf8') as my_file:
+        my_file.write(books_json_parameters)
+
+if __name__ == "__main__":
+    main()
---
+++ tools.py
@@ -0,0 +1,72 @@
+import os
+import requests
+from pathlib import Path
+
+from urllib.parse import urljoin
+from urllib.parse import urlsplit
+from bs4 import BeautifulSoup
+from pathvalidate import sanitize_filename
+
+
+def download_book(book_id, response, title, dest_folder):
+    full_path = f'{dest_folder}/books'
+
+    Path(full_path).mkdir(parents=True, exist_ok=True)
+
+    book_filename = sanitize_filename(f"{book_id}.{title}.txt")
+    book_filepath = os.path.join(full_path, book_filename)
+
+    with open(book_filepath, 'wb') as file:
+       file.write(response.content)
+    return book_filepath
+
+
+def download_image(img_url, dest_folder):
+    full_path = f'{dest_folder}/images'
+
+    Path(full_path).mkdir(parents=True, exist_ok=True)
+
+    img_filename = urlsplit(img_url)[2]
+    img_filename = img_filename.split('/')[-1]
+    img_filepath = os.path.join(full_path, img_filename)
+
+    response = requests.get(img_url)
+
+    with open(img_filepath, 'wb') as file:
+        file.write(response.content)
+    return img_filepath
+
+
+def parse_book_page(answer, book_url):
+    soup = BeautifulSoup(answer.text, 'lxml')
+    comments = []
+    raw_genres = soup.select(".d_book")[1].text
+    genre = raw_genres.split(':')[1].strip()
+    genre = genre[:-1].split(',')
+    
+    title_text = soup.select_one("#content h1").text
+    title = title_text.split('::')
+    title = title[0].strip()
+
+    author = soup.select_one("#content h1 a").text
+
+    raw_img = soup.select_one(".bookimage img")["src"]
+    img_url = urljoin(book_url, raw_img)
+
+    raw_comment = soup.select("#content .texts")
+    for comment in raw_comment:
+        comment = comment.select_one("span").text
+        comments.append(comment)
+    book_parameters = {
+            'title': title,
+            'author' : author,
+            'img_url': img_url,
+            'genres' : genre,
+            'comments' : comments
+            }
+    return book_parameters
+
+
+def check_for_redirect(response):
+    if response.history:
+        raise requests.exceptions.HTTPError
