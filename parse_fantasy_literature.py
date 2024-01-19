36456493a415153b813ae0d1238732528600f4eb
Tkaz <73423742+tw1stzz1@users.noreply.github.com>
2024-01-19 17:57:34 +0300

    add more error cheks

--- parse_fantasy_literature.py
+++ parse_fantasy_literature.py
@@ -23,56 +23,62 @@ def main():
     dest_folder = args.dest_folder
     for numbers in range(int(args.start_page), int(args.end_page)):
         url = f"https://tululu.org/l55/{numbers}/"
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, 'lxml')
-        books_json_parameters = []
-        books = soup.find_all(class_='d_book')
         try:
-            for book_id in books:
-                book_id = book_id.find('a')['href']
-                book_url = urljoin(url, book_id)
-                book_id = book_id[2:-1]
+            response = requests.get(url)
+            check_for_redirect(response)
+            soup = BeautifulSoup(response.text, 'lxml')
+            books_json_parameters = []
+            books = soup.find_all(class_='d_book')
+            try:
+                for book_id in books:
+                    book_id = book_id.find('a')['href']
+                    book_url = urljoin(url, book_id)
+                    book_id = book_id[2:-1]
 
-                answer = requests.get(book_url)
-                answer.raise_for_status()
-                check_for_redirect(answer)
-                
-                book_parameters = parse_book_page(answer, book_url)
-                title = book_parameters['title']
-                img_url = book_parameters['img_url']
-                if not args.skip_imgs:
-                    img_path = download_image(img_url, dest_folder)
-                else:
-                    img_path = None
-                
+                    answer = requests.get(book_url)
+                    answer.raise_for_status()
+                    check_for_redirect(answer)
+                    
+                    book_parameters = parse_book_page(answer, book_url)
+                    title = book_parameters['title']
+                    img_url = book_parameters['img_url']
+                    if not args.skip_imgs:
+                        img_path = download_image(img_url, dest_folder)
+                    else:
+                        img_path = None
+                    
 
-                if not args.skip_txt:
-                    params = {
-                        "id": book_id
+                    if not args.skip_txt:
+                        params = {
+                            "id": book_id
+                        }
+                        response = requests.get(book_txt_url, params=params)
+                        check_for_redirect(response)
+
+                        book_path = download_book(book_id, response, title, dest_folder)
+                    else:
+                        book_path = None
+                    
+                    book_json_parameters = {
+                    'title': title,
+                    'author' : book_parameters['author'],
+                    'img_src' : img_path,
+                    'book_path' : book_path,
+                    'genres' : book_parameters['genres'],
+                    'comments' : book_parameters['comments']
                     }
-                    response = requests.get(book_txt_url, params=params)
-                    check_for_redirect(response)
-
-                    book_path = download_book(book_id, response, title, dest_folder)
-                else:
-                    book_path = None
-                
-                book_json_parameters = {
-                'title': title,
-                'author' : book_parameters['author'],
-                'img_src' : img_path,
-                'book_path' : book_path,
-                'genres' : book_parameters['genres'],
-                'comments' : book_parameters['comments']
-                }
-                
-                books_json_parameters.append(book_json_parameters)
-        except requests.exceptions.ConnectionError:
-            print("Разрыв соединения c сайтом")
-            time.sleep(20)
+                    
+                    books_json_parameters.append(book_json_parameters)
+            except requests.exceptions.ConnectionError:
+                print("Разрыв соединения c сайтом")
+                time.sleep(20)
+            except requests.exceptions.HTTPError:
+                print("Такой книги нет!", book_id)
         except requests.exceptions.HTTPError:
-            print("Такой книги нет!", book_id)
-
+                print("Такой страницы нет!")
+        except requests.exceptions.HTTPError:
+                print("Разрыв соединения c сайтом")
+                time.sleep(20)
 
     books_json_parameters = json.dumps(books_json_parameters, ensure_ascii=False)
 
