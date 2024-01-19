4878ca7dd6103a7ec9a81cd80dc62f2d05dded56
Tkaz <73423742+tw1stzz1@users.noreply.github.com>
2024-01-19 17:27:16 +0300

    Update parse_business_literature.py

--- parse_business_literature.py
+++ parse_business_literature.py
@@ -9,10 +9,11 @@ def main():
     parser = argparse.ArgumentParser(description='This code allows you to download books and their covers form tululu')
     parser.add_argument('--start_id', default=1, help='Id of the book from which the download will begin')
     parser.add_argument('--end_id', default=11, help='Id of the book where the download will end')
+    parser.add_argument('--dest_folder', default="result", help='This setting skips txt file download')
     args = parser.parse_args()
 
     book_txt_url = "https://tululu.org/txt.php"
-
+    dest_folder = args.dest_folder
     for book_id in range(int(args.start_id), int(args.end_id)):
         params = {
             "id": book_id
@@ -31,9 +32,9 @@ def main():
             book_parameters = parse_book_page(answer, book_url)
             title = book_parameters['title']
             img_url = book_parameters['img_url']
-            download_image(img_url)
+            download_image(img_url, dest_folder)
 
-            download_book(book_id, response, title)
+            download_book(book_id, response, title, dest_folder)
         except requests.exceptions.ConnectionError:
             print("Разрыв соединения c сайтом")
             time.sleep(20)
